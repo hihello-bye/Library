e9b9f276f259ceeefcbcd6392b475f9a54e36c4e
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-08-18 15:50:11 +0100

    create book class
    defines structure of a book object

---
+++ script.js
@@ -0,0 +1,11 @@
+class Book {
+    constructor(title, author, genre, pages) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.pages = pages;
+    }
+    printSummary() {
+        return `${this.title} by ${this.author} Genre: ${this.genre} Number of pages: ${this.pages}`;
+    }
+}
